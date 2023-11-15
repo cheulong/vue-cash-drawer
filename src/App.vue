4dee65af5820a0e91dfbc04d797e23f67ed9f2b1
Cheulong Sear <csear@minor.com>
2023-11-15 17:53:44 +0700

    handle INSUFFICIENT_FUNDS and CLOSED

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import CashRegister from './components/CashRegister.vue'
+import { ref } from 'vue'
+
+const cid = ref([
+  ["ONE", 1],
+  ["TWO", 4],
+  ["FIVE", 5],
+  // ["TEN", 60],
+  // ["TWENTY", 100],
+  // ["FIFTY", 50],
+  // ["HUNDRED", 200],
+])
+
+const price = ref(25)
+const cash = ref(30)
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Cash Drawer</h1>
+  <CashRegister :price="price" :cash="cash" :cid="cid" />
 </template>
 
 <style scoped>
@@ -21,9 +29,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ src/components/CashRegister.vue
@@ -0,0 +1,101 @@
+<script setup>
+import { ref } from 'vue'
+
+const {
+  price,
+  cash,
+  cid } = defineProps({
+    price: Number,
+    cash: Number,
+    cid: Array
+  })
+
+const status = ref('')
+const remainChange = ref([])
+const changeArray = ref([])
+function handleRemainCID(remainCID, index, returnedAmount) {
+  return remainCID.map((c, i) => (i === index ? [c[0], c[1] - returnedAmount] : c));
+}
+function isInsufficientFunds(totalCID, change) {
+  return change > totalCID;
+}
+function isDrawerClosed(totalCID, change) {
+  return totalCID == change;
+}
+
+function calculateChange(cid, cash, price) {
+  const currencyUnits = [
+    { name: "ONE", value: 1 },
+    { name: "TWO", value: 2 },
+    { name: "FIVE", value: 5 },
+    { name: "TEN", value: 10 },
+    { name: "TWENTY", value: 20 },
+    { name: "FIFTY", value: 50 },
+    { name: "HUNDRED", value: 100 },
+  ];
+
+  const Change = cash - price;
+  let changeArray1 = [];
+  let remainChange = Change;
+  let remainCID = [...cid]
+  let totalCID = 0;
+
+  for (const unit of cid) {
+    totalCID += unit[1];
+  }
+
+  for (let i = currencyUnits.length - 1; i >= 0; i--) {
+    const { name, value } = currencyUnits[i];
+    const availableUnit = remainCID.find((item) => item[0] === name);
+
+    if (availableUnit) {
+      const maxUnits = Math.floor(availableUnit[1] / value);
+      const returnedUnits = Math.min(maxUnits, Math.floor(remainChange / value));
+      const returnedAmount = returnedUnits * value;
+
+      if (returnedUnits > 0) {
+        changeArray1.push([name, returnedAmount]);
+        remainChange = remainChange - returnedAmount;
+        remainCID = handleRemainCID(remainCID, i, returnedAmount);
+      }
+    }
+  }
+  if (isInsufficientFunds(totalCID, Change)) {
+    status.value = "INSUFFICIENT_FUNDS";
+  } else if (isDrawerClosed(totalCID, Change)) {
+    status.value = "CLOSED";
+    changeArray.value = [...changeArray1];
+  } else {
+    status.value = "OPEN";
+    changeArray.value = [...changeArray1];
+  }
+}
+
+calculateChange(cid, cash, price)
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div>
+    <div v-if="status === 'INSUFFICIENT_FUNDS'">
+      <p>Status: INSUFFICIENT_FUNDS</p>
+      <p>Change: []</p>
+    </div>
+    <div v-else-if="status === 'CLOSED'">
+      <p>Status: CLOSED</p>
+      <p>Change: {{ JSON.stringify(changeArray) }}</p>
+    </div>
+    <div v-else>
+      <p>Status: OPEN</p>
+      <p>Change: {{ JSON.stringify(changeArray) }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
